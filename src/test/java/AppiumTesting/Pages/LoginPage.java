3ee4b2f03cfac4b82f63c4a78d54ef3ed01f5760
Andreanov Ridhovan <andreanovr@gmail.com>
2022-12-24 11:51:04 +0700

    update fixed empty credentials

--- src/test/java/AppiumTesting/Pages/LoginPage.java
+++ src/test/java/AppiumTesting/Pages/LoginPage.java
@@ -1,12 +1,17 @@
 package AppiumTesting.Pages;
 
+import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
+import org.testng.Assert;
 
 public class LoginPage{
+
+    AndroidDriver driver;
 
     By username = By.id("user-name");
     By password = By.id("password");
     By BTNlogin = By.id("login-button");
+    By errorMsg = By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3");
 
     public void setUsername(){
 
@@ -16,5 +21,11 @@ public class LoginPage{
     }
     public void clickBTNlogin(){
 
+    }
+
+    public void ErrorMsg(){
+        String expectedString = "Username is required";
+        String actualString = driver.findElement(errorMsg).getText();
+        Assert.assertTrue(actualString.contains(expectedString));
     }
 }
